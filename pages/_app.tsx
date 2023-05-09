0149cff9a96a204f298555a66db8892647a4f25a
anthonyc-a <anthonyc.animba@gmail.com>
2023-05-09 22:26:54 +0700

    feat: init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,33 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import Script from "next/script";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Script
+        strategy="afterInteractive"
+        src="https://www.googletagmanager.com/gtag/js?id=G-KM3XR6VKMB"
+      />
+      <Script
+        className="script"
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-KM3XR6VKMB', {
+page_path: window.location.pathname,
+});
+`,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
+
+export default MyApp;
