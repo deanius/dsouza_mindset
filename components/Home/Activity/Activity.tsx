4a08bab4804f2abd780c24291e45596d58f966bb
anthonyc-a <anthonyc.animba@gmail.com>
2023-06-12 19:59:09 +0800

    ..

--- components/Home/Activity/Activity.tsx
+++ components/Home/Activity/Activity.tsx
@@ -11,15 +11,15 @@ const Activity = ({ data }: any) => {
           <h3>Currently Watching:</h3>
           <div className={styles.activityItem}>
             <div className={styles.activityImg}>
-              <img src={data[0].fields.image.fields.file.url} alt="" />
+              <img src={data[0]?.fields?.image.fields.file.url} alt="" />
             </div>
             <div className={styles.itemInfo}>
-              <h4>{data[0].fields.name}</h4>
-              <span>{data[0].fields.type}</span>
+              <h4>{data[0]?.fields?.name}</h4>
+              <span>{data[0]?.fields?.type}</span>
 
               <Button
-                name={data[0].fields.platform}
-                location={data[0].fields.link}
+                name={data[0]?.fields?.platform}
+                location={data[0]?.fields?.link}
               />
             </div>
           </div>
@@ -28,15 +28,15 @@ const Activity = ({ data }: any) => {
           <h3>Currently Reading:</h3>
           <div className={styles.activityItem}>
             <div className={styles.activityImg}>
-              <img src={data[1].fields.image.fields.file.url} alt="" />
+              <img src={data[1]?.fields?.image.fields.file.url} alt="" />
             </div>
             <div className={styles.itemInfo}>
-              <h4>{data[1].fields.name}</h4>
-              <span>{data[1].fields.type}</span>
+              <h4>{data[1]?.fields?.name}</h4>
+              <span>{data[1]?.fields?.type}</span>
 
               <Button
-                name={data[1].fields.platform}
-                location={data[1].fields.link}
+                name={data[1]?.fields?.platform}
+                location={data[1]?.fields?.link}
               />
             </div>
           </div>
@@ -47,15 +47,15 @@ const Activity = ({ data }: any) => {
           <h3>Currently Listening:</h3>
           <div className={styles.activityItem}>
             <div className={styles.activityImg}>
-              <img src={data[2].fields.image.fields.file.url} alt="" />
+              <img src={data[2]?.fields?.image.fields.file.url} alt="" />
             </div>
             <div className={styles.itemInfo}>
-              <h4>{data[2].fields.name}</h4>
-              <span>{data[2].fields.type}</span>
+              <h4>{data[2]?.fields?.name}</h4>
+              <span>{data[2]?.fields?.type}</span>
 
               <Button
-                name={data[2].fields.platform}
-                location={data[2].fields.link}
+                name={data[2]?.fields?.platform}
+                location={data[2]?.fields?.link}
               />
             </div>
           </div>
@@ -64,15 +64,15 @@ const Activity = ({ data }: any) => {
           <h3></h3>
           <div className={styles.activityItem}>
             <div className={styles.activityImg}>
-              <img src={data[3].fields.image.fields.file.url} alt="" />
+              <img src={data[3]?.fields?.image.fields.file.url} alt="" />
             </div>
             <div className={styles.itemInfo}>
-              <h4>{data[3].fields.name}</h4>
-              <span>{data[3].fields.type}</span>
+              <h4>{data[3]?.fields?.name}</h4>
+              <span>{data[3]?.fields?.type}</span>
 
               <Button
-                name={data[3].fields.platform}
-                location={data[3].fields.link}
+                name={data[3]?.fields?.platform}
+                location={data[3]?.fields?.link}
               />
             </div>
           </div>
