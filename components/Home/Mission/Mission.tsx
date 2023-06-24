4c2595ced5eb6e4f0ef79f35d6ee3c1fe4191f57
Dean Dsouza <deandsouza@Deans-MacBook-Pro.local>
2023-06-24 16:35:36 -0400

    Made certain words bold

--- components/Home/Mission/Mission.tsx
+++ components/Home/Mission/Mission.tsx
@@ -6,10 +6,10 @@ const Mission = () => {
     <div className={styles.mission}>
       <h2>Join us as we embark on a transformative journey of self-discovery and embrace the beauty of being human.</h2>
       <p>
-      With The Dean Dsouza Mindset, our mission is to inspire and empower individuals on their journey to wellness, positivity, and mental health. 
-      We believe that true strength lies in embracing ourselves for who we are, strengths and flaws.
-      We aim to illuminate the power of positivity and hope, as extraordinary forces that can transform lives. 
-      Through our platform, we strive to dismantle the stigma surrounding mental health and advocate for the exploration of vulnerabilities,
+      With The Dean Dsouza Mindset, our mission is to <b>inspire and empower</b> individuals on their journey to wellness, positivity, and mental health. 
+      We believe that true strength lies in embracing ourselves for who we are, <b>strengths and flaws</b>.
+      We aim to illuminate the power of <b>positivity and hope</b>, as extraordinary forces that can transform lives. 
+      Through our platform, we strive to dismantle the stigma surrounding <b>mental health</b> and advocate for the exploration of <b>vulnerabilities</b>,
       as we believe it is a path towards personal growth and resilience. 
       </p>
     </div>
