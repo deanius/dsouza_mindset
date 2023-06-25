ac4a53195f25d78f7eb558f4e3bd0faead6a1d83
Dean Dsouza <deandsouza@Deans-MacBook-Pro.local>
2023-06-24 22:00:46 -0400

    Modified the mission statement

--- components/Home/Mission/Mission.tsx
+++ components/Home/Mission/Mission.tsx
@@ -4,13 +4,13 @@ import styles from "./Mission.module.css";
 const Mission = () => {
   return (
     <div className={styles.mission}>
-      <h2>Join us as we embark on a transformative journey of self-discovery and embrace the beauty of being human.</h2>
+      <h2>Mission Statement </h2>
       <p>
       With The Dean Dsouza Mindset, our mission is to <b>inspire and empower</b> individuals on their journey to wellness, positivity, and mental health. 
       We believe that true strength lies in embracing ourselves for who we are, <b>strengths and flaws</b>.
       We aim to illuminate the power of <b>positivity and hope</b>, as extraordinary forces that can transform lives. 
       Through our platform, we strive to dismantle the stigma surrounding <b>mental health</b> and advocate for the exploration of <b>vulnerabilities</b>,
-      as we believe it is a path towards personal growth and resilience. 
+      as we believe it is a path towards personal growth and resilience. Join us as we embark on a transformative journey of self-discovery and embrace the beauty of being human.
       </p>
     </div>
   );
