b47852abe513ecd9764575610dde9b13f6d98ae3
Dean Dsouza <deandsouza@Deans-MacBook-Pro.local>
2023-07-10 19:02:28 -0400

    Moved the placement of Support me buttons

--- components/Support/SupportMeButtons/SupportMeButtons.tsx
+++ components/Support/SupportMeButtons/SupportMeButtons.tsx
@@ -5,12 +5,20 @@ const SupportMeButtons = ({ active }: any) => {
   return (
     <div className={styles.supportMeButtons}>
       <a
+        href="https://wishlist.com/l/gZkpZN"
+        className={styles.wishlist}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        My Wishlist
+      </a>
+      <a
         href="/hi"
         className={styles.patreon}
         target="_blank"
         rel="noopener noreferrer"
       >
-        Patreon - Coming Soon
+        [In Development] Patreon 
       </a>
       <a
         href="/hi"
@@ -18,15 +26,7 @@ const SupportMeButtons = ({ active }: any) => {
         target="_blank"
         rel="noopener noreferrer"
       >
-        GoFundMe - Coming Soon
-      </a>
-      <a
-        href="https://wishlist.com/l/gZkpZN"
-        className={styles.wishlist}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        My Wishlist
+        [In Development] GoFundMe
       </a>
     </div>
   );
